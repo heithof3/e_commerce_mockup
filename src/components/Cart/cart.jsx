0e6e67a0fb16bb38d9a9550a058766b1856696bb
heithof3 <heith.robbins@gmail.com>
2021-08-16 19:07:23 -0600

    first commit

---
+++ src/components/Cart/cart.jsx
@@ -0,0 +1,47 @@
+import { Container, Typography, Button, Grid } from '@material-ui/core';
+
+
+import useStyles from './cartStyles';
+
+
+const Cart = ({ cart }) => {
+    const classes = useStyles();
+
+    const EmptyCart = () => {
+        <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+    }
+
+    const FilledCart = () => {
+        <>
+            <Grid Container spacing={3}>
+                {cart.line_items.map((item) => (
+                    <Grid item xs={12} sm={4} key={item.id}>
+                        <div>{item.name}</div>
+                    </Grid>
+                ))}
+            </Grid>
+            <div className={classes.cardDetails}>
+                    <Typography variant="h4">
+                        Subtotal: { cart.subtotal.formatted_with_symbol }
+                    </Typography>
+                    <div>
+                        <Button className={classes.emptyButton} size='large' type="button" variant='contained' color='secondary'>Empty Cart</Button>
+                        <Button className={classes.checkoutButton} size='large' type="button" variant='contained' color='primary'>CheckoutButton</Button>
+                    </div>
+            </div>
+        </>
+    };
+
+    if(!cart.line_items) return 'Loading...';
+
+        return (
+            <Container>
+            <div className={classes.toolbar} />
+            <Typography className={classes.title} varaint="h3">Your Shopping Cart</Typography>
+            { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+            </Container>
+        )
+    
+}
+
+export default Cart;
